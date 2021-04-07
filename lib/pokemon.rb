b787ea7cfb891b070f569572622f14988352cc31
github username <github email address>
2021-04-07 14:11:05 -0400

    commit tree b486171b052f8161bfbd6c458771e7ad0a6a91a2

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,9 +14,20 @@ class Pokemon
         INSERT INTO pokemon (name, type)
         VALUES (?,?)
         SQL
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
 
-        @db.execute(sql, self.name, self.type)
-        @id = @db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    def self.find(id, db)
+        sql = <<-SQL 
+        SELECT * 
+        FROM pokemon
+        WHERE id = ?
+        SQL
+
+        result = db.execute(sql, id)[0]
+        Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+
     end
 
 end
